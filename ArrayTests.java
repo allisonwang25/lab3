d8cc59d38bd00a1702e4b401e6c9ad55d187398e
allisonwang25 <allisonwang25@gmail.com>
2024-01-31 21:05:51 -0800

    Fix Bug in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,10 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedMultipleItemArray() {
+    int[] input1 = {3,4,5};
+    assertArrayEquals(new int[]{5,4,3}, ArrayExamples.reversed(input1));
+  }
 }
